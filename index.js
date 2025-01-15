13a0c97db4a83a100dfcf63e66ebb518e7ad7cfe
Arjun-JP <arjunjpscout@gmail.com>
2025-01-15 15:22:38 +0530

    update

--- index.js
+++ index.js
@@ -37,6 +37,10 @@ app.use("/warehouse", warehouseRoutes);
 app.use("/auth", authRoutes);
 
 // Start the server
+// http.listen(3000, function () {
+//   console.log("Server running on http://localhost:3000");
+// });
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
