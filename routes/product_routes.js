2b5db914a8009238354eb6a1bb3b80db05f04ab4
Arjun-JP <arjunjpscout@gmail.com>
2025-02-10 11:04:59 +0530

    add end point to get multiple subcategory products

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -11,6 +11,10 @@ router.put("/update/:productId", ProductController.updateProduct);
 router.delete("/:productId", ProductController.deleteProduct);
 router.put("/updateStock/:productId", ProductController.updateStock);
 router.post("/addReview/:productId", ProductController.addReview);
+router.post(
+  "/products-by-subcategories",
+  ProductController.getProductsBySubCategories
+);
 router.get("/get/Drafts", ProductController.getDrafts);
 router.get("/get/lowStockProduct", ProductController.getLowStockProducts);
 module.exports = router;
