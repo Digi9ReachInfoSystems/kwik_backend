e7a604bd9d5c1211fd985147cc73f573d27ab4ba
jayanthbr29 <jayanthbr@digi9.co.in>
2025-02-10 16:30:40 +0530

    brand api

--- controller/brand_controller.js
+++ controller/brand_controller.js
@@ -14,28 +14,12 @@ exports.getAllBrands = async (req, res) => {
 // Add a new brand
 exports.addBrand = async (req, res) => {
   try {
-    const { brand_id, brand_name, brand_image, brand_des, brand_url } =
+    const { brand_id, brand_name, brand_image, brand_des, brand_url ,color} =
       req.body;
 
     // Validate required fields
     if (!brand_id || !brand_name || !brand_image || !brand_des) {
       return res.status(400).json({ message: "Missing required fields" });
-    }
-
-    // Validate brand_id uniqueness
-    const existingBrand = await Brand.findOne({ brand_id });
-    if (existingBrand) {
-      return res.status(400).json({ message: "Brand ID must be unique" });
-    }
-
-    // Validate image URL
-    if (!validator.isURL(brand_image)) {
-      return res.status(400).json({ message: "Invalid brand image URL" });
-    }
-
-    // Validate optional brand_url URL
-    if (brand_url && !validator.isURL(brand_url)) {
-      return res.status(400).json({ message: "Invalid brand URL" });
     }
 
     const newBrand = new Brand({
@@ -44,6 +28,7 @@ exports.addBrand = async (req, res) => {
       brand_image,
       brand_des,
       brand_url,
+      color
     });
 
     const savedBrand = await newBrand.save();
@@ -62,16 +47,6 @@ exports.editBrand = async (req, res) => {
   try {
     const brandId = req.params.id; // MongoDB Object ID of the brand to edit
     const updatedData = req.body;
-
-    // Validate brand image URL
-    if (updatedData.brand_image && !validator.isURL(updatedData.brand_image)) {
-      return res.status(400).json({ message: "Invalid brand image URL" });
-    }
-
-    // Validate brand URL
-    if (updatedData.brand_url && !validator.isURL(updatedData.brand_url)) {
-      return res.status(400).json({ message: "Invalid brand URL" });
-    }
 
     const updatedBrand = await Brand.findByIdAndUpdate(brandId, updatedData, {
       new: true, // Return the updated document
