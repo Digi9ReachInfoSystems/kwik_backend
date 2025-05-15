e345dc5062a07cad9905d39a6b5eb8b40cd3ba0f
jayanthbr29 <jayanthbr@digi9.co.in>
2025-05-15 18:56:26 +0530

    console blog

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -144,6 +144,7 @@ exports.verifyPayment = async (req, res) => {
             userOrder.order_placed_time = Date.now();
             userOrder.coupon_ref=coupon._id;
             await userOrder.save();
+            console.log("userOrder", req.body.payload.payment.entity.notes.user_id);
             const user = await User.findById(req.body.payload.payment.entity.notes.user_id );
             user.cart_products = [];
             await user.save();
